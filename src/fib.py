3478e9cbbbe6ccb8cb28a14ad49391c8b1fb95aa
Katherine Kumar <Katherine2bk@gmail.com>
2021-10-20 08:52:00 -0700

    set up recursion problem... set up notes.py

---
+++ src/fib.py
@@ -0,0 +1,16 @@
+# Write a function 'fib(n)' that takes in a number as an argument.
+# The function should return the n-th number of the Fibonacci sequence.
+
+# The first and second number of the sequence is 1.
+# To generate the next number of the sequence, we sum the previous two.
+
+def fib(n):
+    if n <= 2:
+        return 1
+    return fib(n - 1) + fib(n -2)
+
+print(fib(6))
+print(fib(50))
+
+# if our argument is too big, it can cause issues, we need to further =>
+# improve our algorithm
