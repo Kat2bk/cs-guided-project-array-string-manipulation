60f4819c523c08fa054214c5df35da5b9288de6e
Katherine Kumar <Katherine2bk@gmail.com>
2021-10-21 10:07:21 -0700

    more notes

--- src/fib.py
+++ src/fib.py
@@ -25,8 +25,17 @@ def fib(n, memo = {}):
         return memo[n]
     if n <= 2:
         return 1
-    return fib(n -1) + fib(n - 2)
+    memo[n] = fib(n -1, memo) + fib(n - 2, memo) #storing inside memo
+    return memo[n]
 
 print(fib(6))
 print(fib(20))
-# print(fib(50))
+print(fib(50))
+
+# when it first runs it initializes the dict
+# when it calls the function back through the callstack (recursive call)
+# it stores the values of the parents inside the dict
+# 3: 2, 4: 3
+# if it sees that the key of 3 is already inside the memo, it will just return
+# the stored value of 2
+# it doesn't have to travel all the way down the node tree
