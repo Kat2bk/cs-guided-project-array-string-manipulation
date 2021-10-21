8bc1de70d3d96985d0710c7a1f7bc7467d66358a
Katherine Kumar <Katherine2bk@gmail.com>
2021-10-21 08:48:17 -0700

    added more notes to fib.py.

--- src/fib.py
+++ src/fib.py
@@ -4,13 +4,29 @@
 # The first and second number of the sequence is 1.
 # To generate the next number of the sequence, we sum the previous two.
 
-def fib(n):
-    if n <= 2:
-        return 1
-    return fib(n - 1) + fib(n -2)
-
-print(fib(6))
-print(fib(50))
+# def fib(n):
+#     if n <= 2:
+#         return 1
+#     return fib(n - 1) + fib(n -2)
+# '''''''
+# print(fib(6))
+# print(fib(50))
 
 # if our argument is too big, it can cause issues, we need to further =>
 # improve our algorithm
+
+# memoization
+# reminder, capturing data to use later
+# object for JS, dictionary for Python
+# keys will be arg, value will be return value
+
+def fib(n, memo = {}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    return fib(n -1) + fib(n - 2)
+
+print(fib(6))
+print(fib(20))
+# print(fib(50))
