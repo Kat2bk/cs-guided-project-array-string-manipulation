dcc6ca36d05384f5725a545d69bf9e70a97e0c0b
Katherine Kumar <Katherine2bk@gmail.com>
2021-11-02 16:47:37 -0700

    added more notes about mutablity... runtime

--- src/notes.py
+++ src/notes.py
@@ -38,3 +38,33 @@
 
 # 3 tests cases:
 # Pivot Index
+# [1, 3, 4, 5, 2, 1] = 4
+# [1, 5, 3, 6, 3, 1] = -1
+# [5, 5, 5, 5, 5, 5] = -1
+
+# Immutable/Mutables
+# RAM Computers use it all the time to store variables
+# it stores it within RAM and when it is called, it retrieves it
+# num2 = num will reference the same place in memory
+# however, if you assign it to a different value, it will change it
+# anytime you change the value, it will delete the old reference and create a new one
+# this is immutable values/variables, they are easy to work with... integers, string
+# this isn't true for everything
+# arr = [1, 2, 3]
+# arr2 = arr
+# both reference same box, however, if you append something to it, it changes both
+# mutable variables ... arrays, class instance, objects
+# creating a new array every time is expensive in space
+# you have to keep track of where you assign things
+# a dictionary is ... mutable
+# there's a difference between appending and assigning
+# appending will change the original array, assigning will not
+# there are no methods that modifies a string
+
+# Arrays: (What they are in memory)
+# Static Array -- we don't deal with these too much
+# access -- print(arr[0])
+# constant runtime to access items in array O(1)
+# however, if you want to append it's O(n)
+# 
+# 
